0ca56a88d09b0deed822e6b6325b09a422f6396f
KIruba <kirupanithi1@gmail.com>
2026-07-01 21:20:14 -0700

    this file has code for label break

---
+++ src/day7/classwork/labelbreak.java
@@ -0,0 +1,16 @@
+package day7.classwork;
+
+public class labelbreak {
+
+        public static void main(String[] args) {
+            for(int week=1;week<=4;week++){
+                for(int day=1;day<=7;day++){
+                    if(day==3){break;
+                    }
+                    System.out.println(" week : " +week +" day : "+day);
+                }
+
+            }
+        }
+    }
+
