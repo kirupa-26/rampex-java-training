1c141dd0facd0d80452db64647e105e02ccc8e17
KIruba <kirupanithi1@gmail.com>
2026-07-01 21:14:18 -0700

    this file has code for array

---
+++ src/day7/classwork/array.java
@@ -0,0 +1,22 @@
+package day7.classwork;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class array {
+
+
+
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int[] arr=new int[5];
+
+            for (int i = 0; i < 5; i++) {
+                System.out.println("enter the position:" + (i+1));
+                arr[i] = sc.nextInt();
+            }
+            System.out.println(Arrays.toString(arr));
+
+        }
+    }
+
