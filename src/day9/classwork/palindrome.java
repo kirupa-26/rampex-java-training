dc017c238ee9a49b80344f2a1809aaf387a9f79b
KIruba <kirupanithi1@gmail.com>
2026-07-01 21:57:41 -0700

    this file has code for  string

---
+++ src/day9/classwork/palindrome.java
@@ -0,0 +1,23 @@
+package day9.classwork;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class palindrome {
+
+
+
+        public static void main(String[] args) {
+            String s="madam";
+
+
+            char[] ch=new char[s.length()];
+            int n=s.length();
+
+            for(int i=0;i<s.length();i++){
+                ch[i]=s.charAt(n-1-i);
+            }
+            System.out.print(Arrays.toString(ch));
+        }
+    }
+
