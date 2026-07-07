55195db77dd8e89632478af30812531ce7c587f2
KIruba <kirupanithi1@gmail.com>
2026-07-07 02:03:09 -0700

    this file has code for Bankingtst

---
+++ src/day11/hometask/AssignmentExpression.java
@@ -0,0 +1,15 @@
+package day11.hometask;
+
+public class AssignmentExpression {
+
+         public static void main(String[] args){
+            int x = 5;
+            if (x > 10 && ++x > 5)
+            {
+            }
+            System.out.println(x);
+        }
+    }
+
+
+
