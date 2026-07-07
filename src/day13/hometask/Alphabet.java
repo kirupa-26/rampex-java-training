be235e8d4c96533ab49821ee133563b382328dc6
KIruba <kirupanithi1@gmail.com>
2026-07-07 03:15:21 -0700

    this file has code for car

---
+++ src/day13/hometask/Alphabet.java
@@ -0,0 +1,19 @@
+package day13.hometask;
+
+public class Alphabet {
+
+    public static void main(String[] args) {
+        int rows = 6;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");}
+            for (int j = 0; j < i; j++) {
+                System.out.print((char)('A' + j) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
